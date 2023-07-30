e8887ffb4afc37b9995f8c759b393758d0336ff0
Shaoting-Yan <112460516+Shaoting-Yan@users.noreply.github.com>
2023-07-31 05:44:11 +0800

    pillars

---
+++ pendulum/draw.js
@@ -0,0 +1,76 @@
+function pillars(c,m,r){
+    let cell = (windowWidth-2*r-2*m)/c;
+    let fullcount = int(PI*camHeight/cell);
+    for(i=-fullcount;i<fullcount;i+=1){
+      for(j=-20;j<20;j+=1){
+        push();
+        translate(i*cell,j*cell,r*2);
+        rotateX(HALF_PI);
+        cylinder(r,r*4);
+        pop();
+      }
+    }
+    toplevel = r*5;
+  }
+
+function dots(c,m,r){
+    let cell = (windowWidth-2*r-2*m)/c;
+    let fullcount = int(PI*camHeight/cell);
+    for(i=-fullcount;i<fullcount;i+=1){
+      for(j=-20;j<20;j+=1){
+        push();
+        translate(i*cell,j*cell,0);
+        circle(0,0,r*2);
+        pop();
+      }
+    }
+    toplevel = r*5;
+  }
+
+  function boxs(c,m,r){
+    let cell = (windowWidth-2*r-2*m)/c;
+    for(i=-20;i<20;i+=1){
+      for(j=-20;j<20;j+=1){
+        push();
+        translate(i*cell,j*cell,r*2);
+        box(r*2,r*2,r*2);
+        pop();
+      }
+    }
+    toplevel = r*3;
+  }
+  
+  function buttons(c,m,r){
+    for(i=-20;i<20;i+=1){
+      for(j=-20;j<20;j+=1){
+        push();
+        colorMode(HSB,255);
+        var currColor = color(randomColor(i,j));
+        fill(dimmer(currColor,0.5));
+  
+        translate(i*cell,j*cell,0);
+        rotateX(HALF_PI);
+  
+        cylinder(r,r*4,24,1,false,false);
+  
+        rotateX(-HALF_PI);
+        translate(0,0,r*2);
+        
+        fill(currColor);
+  
+        circle(0,0,r*2);
+        pop();
+      }
+    }
+  }
+
+  function showdata(toplevel, messages){
+    push();
+    fill('red');
+    textSize(20);
+    translate(0,0,toplevel);
+    for(let i = 0; i< messages.length; i++){
+      text(messages[i].toFixed(2), 10, i*25);
+    }    
+    pop();
+  }
---
+++ pendulum/sketch.js
@@ -0,0 +1,90 @@
+p5.disableFriendlyErrors = true;
+let permissionGranted = false;
+
+//for the shader;
+let layer, Shader;
+//for camera integrate
+let accX, accY, accZ;
+let currRx = 0;
+
+function showUI(){
+  let gap = 40;
+  heave = createSlider(1, 6, 3, 0);
+  heave.position(120, 500);
+  heave.style('width', '200px');
+  sway = createSlider(1, 6, 3, 0);
+  sway.position(120, 500+gap);
+  sway.style('width', '200px');
+  surge = createSlider(1, 6, 3, 0);
+  surge.position(120, 500+gap*2);
+  surge.style('width', '200px');
+  damp = createSlider(1, 6, 3, 0);
+  damp.position(120, 500+gap*3);
+  damp.style('width', '200px');
+
+  p1 = createP('heave');
+  p1.style('font-size', '20px');
+  p1.position(330, 475);
+  p2 = createP('sway');
+  p2.style('font-size', '20px');
+  p2.position(330, 475+gap);
+  p3 = createP('surge');
+  p3.style('font-size', '20px');
+  p3.position(330, 475+gap*2);
+  p4 = createP('damp');
+  p4.style('font-size', '20px');
+  p4.position(330, 475+gap*3);
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight,WEBGL);
+  textFont(helvetica);
+
+  background('darkslategray');
+
+  camHeight = height/2/tan(PI/6);
+  //framebuffer
+  layer = createFramebuffer();
+  
+  showUI();
+}
+
+function draw() {
+
+  let rotation = getEulerAngles(getRotationMatrix(rotationZ,rotationX,rotationY));
+  Rz = rotation[0];
+  Ry = rotation[2];
+  Rx = rotation[1];  
+  
+  he = heave.value();
+  sw = sway.value();
+  su = surge.value();
+  da = damp.value();
+
+  //start framebuffer
+  layer.begin();
+
+  clear();
+  noStroke();
+  background(0);
+  lights();
+
+  push();
+
+  moveObject(Rx,Ry,Rz);
+  pillars(3,-25,windowWidth/12); //(cell,margin,radius)
+
+  pop();
+
+  moveCamera(accelerationX,accelerationY,accelerationZ);
+
+  showdata(200,[heave.value(),sway.value(),surge.value()]);
+
+  layer.end(); //end frame buffer
+
+  //apply depth buffer
+  shader(Shader);
+  Shader.setUniform('depth', layer.depth);
+  Shader.setUniform('img', layer.color);
+  rect(0,0,width,height);
+}
