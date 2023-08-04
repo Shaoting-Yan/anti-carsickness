1c0f22c1991a454cf7b43ae244dda978230852ad
Shaoting-Yan <112460516+Shaoting-Yan@users.noreply.github.com>
2023-08-04 12:39:02 +0800

    horoshi sugimoto

---
+++ waves - Hiroshi Sugimoto/draw.js
@@ -0,0 +1,147 @@
+function pillars(c,m,r){
+    let cell = (windowWidth-2*r-2*m)/c;
+    for(i=-20;i<20;i+=1){
+      for(j=-20;j<20;j+=1){
+        push();
+        translate(i*cell,j*cell,r*2);
+        rotateX(HALF_PI);
+        cylinder(r,r*4);
+        pop();
+      }
+    }
+    toplevel = r*5;
+  }
+
+  function boxs(c,m,r){
+    let cell = (windowWidth-2*r-2*m)/c;
+    for(i=-20;i<20;i+=1){
+      for(j=-20;j<20;j+=1){
+        push();
+        translate(i*cell,j*cell,r*2);
+        box(r*2,r*2,r*2);
+        pop();
+      }
+    }
+    toplevel = r*3;
+  }
+  
+  function buttons(c,m,r){
+    for(i=-20;i<20;i+=1){
+      for(j=-20;j<20;j+=1){
+        push();
+        colorMode(HSB,255);
+        var currColor = color(randomColor(i,j));
+        fill(dimmer(currColor,0.5));
+  
+        translate(i*cell,j*cell,0);
+        rotateX(HALF_PI);
+  
+        cylinder(r,r*4,24,1,false,false);
+  
+        rotateX(-HALF_PI);
+        translate(0,0,r*2);
+        
+        fill(currColor);
+  
+        circle(0,0,r*2);
+        pop();
+      }
+    }
+  }
+
+  function showdata(toplevel, messages){
+    push();
+    fill('red');
+    textSize(20);
+    translate(0,0,toplevel);
+    for(let i = 0; i< messages.length; i++){
+      text(messages[i].toFixed(2), 10, i*25);
+    }    
+    pop();
+  }
+
+  function dot(){
+    sphere(50,50,50);
+  }
+
+function wave(i){
+  this.yoff = i*0.1;
+  this.xoff = 0;
+  this.render = function render(i,strength,alpha){
+    this.waveh = 10*i*(strength);
+    fill("#"+palette[numLayers-i]);
+    noStroke();
+    // stroke("#"+strokePalette[numLayers-i]);
+    // let weight = map(i/numLayers,0,1,5,2); 
+    // strokeWeight(weight);
+    beginShape();
+    this.xoff = 0;
+    for(let x = -waveWidth; x<=waveWidth;x += precision){
+      y = map(noise(this.xoff, this.yoff),
+                    0,1,
+                    -this.waveh,
+                    this.waveh); /// generate 2-d noise values
+      vertex(x,y); /// construct wave form
+      this.xoff += 0.02;
+    }
+    this.yoff += 0.005;
+    vertex(waveWidth, height); /// complete the wave layer
+    vertex(waveWidth, height); /// tie the last x to the side if not perfect x10 width
+    vertex(-waveWidth, height);
+    endShape(CLOSE);
+  }
+}  
+
+function waves(currX,currY,numLayers){
+  push();
+  rotateZ(HALF_PI-Rz);
+  translate(currX-width/2,50+currY);
+  for(let i = 0;i<numLayers;i++){
+    translate(0,0,i*10);
+    strength = map(abs(currY),0,10,1,2);
+    all[i].render(i+1,strength,100);
+  }
+  pop();
+}
+
+function sun(x,y,p){
+  let r = 50;
+  let changed = r+breath(p,r);
+  push()
+  translate(x,y,-changed);
+  noStroke();
+  fill('#F2C37E');
+  sphere(changed);
+  pop();
+}
+
+function showUI(){
+  shown = true;
+  let gap = 40;
+  heave = createSlider(0.1, 6, he, 0);
+  heave.position(120, 500);
+  heave.style('width', '200px');
+  sway = createSlider(0.1, 6, sw, 0);
+  sway.position(120, 500+gap);
+  sway.style('width', '200px');
+  surge = createSlider(0.1, 6, su, 0);
+  surge.position(120, 500+gap*2);
+  surge.style('width', '200px');
+  damp = createSlider(1, 30, da, 0);
+  damp.position(120, 500+gap*3);
+  damp.style('width', '200px');
+
+  p1 = createP('heave');
+  p1.style('font-size', '20px');
+  p1.position(330, 475);
+  p2 = createP('sway');
+  p2.style('font-size', '20px');
+  p2.position(330, 475+gap);
+  p3 = createP('surge');
+  p3.style('font-size', '20px');
+  p3.position(330, 475+gap*2);
+  p4 = createP('damp');
+  p4.style('font-size', '20px');
+  p4.position(330, 475+gap*3);
+  tweak.remove();
+}
---
+++ waves - Hiroshi Sugimoto/sketch.js
@@ -0,0 +1,109 @@
+p5.disableFriendlyErrors = true;
+let permissionGranted = false;
+
+//for the shader;
+let layer, Shader,contactShadowRenderer;
+
+//for camera integrate
+let accX, accY, accZ;
+let currRx = 0;
+
+//motion parameter
+let he = 3;
+let sw = 3;
+let su = 2;
+let da = 15;
+let shown = false;
+
+//colors
+palette = ['383B34','383B34','4A4F49','5E655E','7B817B','909A92','B1BAB5','D7DDD9'];
+
+//wave parameter
+let precision = 10;
+let all = [];
+let numLayers = 8;
+let vert = 1;
+let waveWidth = 0; 
+
+function setup() {
+  pixelDensity(2);
+  createCanvas(windowWidth,windowHeight,WEBGL);
+  blurRenderer = createGaussianBlurRenderer();
+  blurRenderer.setIntensity(0.1);
+  blurRenderer.setSamples(20);
+  blurRenderer.setDof(50);
+
+  sunLayer = createFramebuffer(width,height,WEBGL);
+
+  textFont(helvetica);
+
+  background('darkslategray');
+
+  camHeight = height/2/tan(PI/6);
+  //framebuffer
+  layer = createFramebuffer();
+  fog = color('white');
+
+  tweak = createButton('tweak');
+  tweak.position(300,600);
+  tweak.mousePressed(showUI);
+
+  //prepare waves
+  for (let i = 0; i < numLayers; i++) {
+    all.push(new wave(i));
+  }
+  waveWidth = width*6;
+
+  let a = createA('https://shaoting-yan.github.io/anti-carsickness/waves-overlay/', 'to pure mode');
+  a.position(10, 10);
+}
+
+function draw() {
+  clear();
+  let rotation = getEulerAngles(getRotationMatrix(rotationZ,rotationX,rotationY));
+  Rz = rotation[0];
+  Ry = rotation[2];
+  Rx = rotation[1];  
+
+  if (shown){
+    he = heave.value();
+    sw = sway.value();
+    su = surge.value();
+    da = damp.value();
+  }
+
+  let currX = Ry*camHeight;
+
+  waves(currX,accY/25,numLayers);
+  moveCamera(accelerationX,accelerationY,accelerationZ);
+
+  // layer.begin();
+  //   clear();
+  //   moveCamera(accelerationX,accelerationY,accelerationZ);
+  //   waves(currX,accY/25,numLayers);
+  // layer.end();
+
+  // shader(fogShader);
+  // fogShader.setUniform('fog', [red(fog), green(fog), blue(fog)]);
+  // fogShader.setUniform('img', layer.color);
+  // fogShader.setUniform('depth', layer.depth);
+  // rect(0, 0, width, height);
+
+  // // blurRenderer.draw(() => {
+  // //   clear();  
+  // //   background(0,0,0,0);
+  // //   moveCamera(accelerationX,accelerationY,accelerationZ);
+
+  // //   let p = 60;
+
+  // //   sun(0,-200+breath(p,300),p);//x,y,period
+
+  // //   waves(currX,accY/25,numLayers);
+  
+  // //   //breathing focus
+  // //   push();
+  // //   translate(0,0,200+breath(10,50)); //focus distance
+  // //   blurRenderer.focusHere();
+  // //   pop();
+  // // });  
+}
